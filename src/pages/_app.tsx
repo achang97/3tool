55eacfb6b519422257cc3c55f85eaed256807070
Andrew Chang <andrew@acalabs.xyz>
2022-12-17 14:36:30 -0500

    Add tests for Auth0RedirectWrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import { ReactNode, useEffect } from 'react';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { Box } from '@mui/material';
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
 import { Toolbar } from '@app/components/toolbar/Toolbar';
@@ -15,32 +15,11 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { WagmiConfig } from 'wagmi';
 import { Web3Modal } from '@web3modal/react';
-import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { Auth0RedirectWrapper } from '@app/components/auth0/Auth0RedirectWrapper';
 
 import '@app/styles/globals.css';
 import '@app/styles/react-grid-layout.css';
-import { FullscreenLoader } from '@app/components/common/FullscreenLoader';
-import Head from 'next/head';
-
-type Auth0RedirectWrapperProps = {
-  children: ReactNode;
-};
-
-const Auth0RedirectWrapper = ({ children }: Auth0RedirectWrapperProps) => {
-  const { isLoading, isAuthenticated, loginWithRedirect } = useAuth0();
-
-  useEffect(() => {
-    if (!isLoading && !isAuthenticated) {
-      loginWithRedirect({ redirectUri: window.location.origin });
-    }
-  }, [isLoading, isAuthenticated, loginWithRedirect]);
-
-  if (isLoading || !isAuthenticated) {
-    return <FullscreenLoader />;
-  }
-
-  return children as JSX.Element;
-};
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
