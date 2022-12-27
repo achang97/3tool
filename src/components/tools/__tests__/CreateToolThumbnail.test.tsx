7bc018000d5f5248c5e691f12351168b027123f1
Andrew Chang <andrew@acalabs.xyz>
2022-12-27 11:17:03 -0800

    Add tests for CreateToolDialog

---
+++ src/components/tools/__tests__/CreateToolThumbnail.test.tsx
@@ -0,0 +1,52 @@
+import { render, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { CreateToolThumbnail } from '../CreateToolThumbnail';
+
+const mockPush = jest.fn();
+const mockCreateTool = jest.fn();
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(() => ({
+    push: mockPush,
+  })),
+}));
+
+jest.mock('@app/redux/services/tools', () => ({
+  useCreateToolMutation: jest.fn(() => [mockCreateTool, {}]),
+}));
+
+describe('CreateToolThumbnail', () => {
+  it('renders description', () => {
+    const result = render(<CreateToolThumbnail />);
+
+    expect(result.getByText('Start a new project')).toBeDefined();
+  });
+
+  it('opens dialog on click', async () => {
+    const result = render(<CreateToolThumbnail />);
+
+    userEvent.click(result.getByText('Start a new project'));
+    await waitFor(() => {
+      expect(result.getByText('Create new tool')).toBeDefined();
+    });
+  });
+
+  it('closes dialog when clicking outside of dialog', async () => {
+    const result = render(
+      <>
+        <CreateToolThumbnail />
+        <div>Close dialog</div>
+      </>
+    );
+
+    userEvent.click(result.getByText('Start a new project'));
+    await waitFor(() => {
+      expect(result.getByText('Create new tool')).toBeDefined();
+    });
+
+    userEvent.click(result.getByText('Close dialog'));
+    await waitFor(() => {
+      expect(result.queryByTestId('Create new tool')).toBeNull();
+    });
+  });
+});
