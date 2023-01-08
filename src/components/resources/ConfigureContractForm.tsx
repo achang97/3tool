93d0143587a97fff3597f492673de7764f7b4fc6
Andrew Chang <andrew@acalabs.xyz>
2023-01-07 17:39:38 -0800

    Add ConfigureContractForm

--- src/components/resources/ConfigureContractForm.tsx
+++ src/components/resources/ConfigureContractForm.tsx
@@ -1,4 +1,11 @@
-import { ChangeEvent, useCallback, useMemo, useState } from 'react';
+import {
+  ChangeEvent,
+  FormEvent,
+  ReactNode,
+  useCallback,
+  useMemo,
+  useState,
+} from 'react';
 import { CHAINS } from '@app/utils/constants';
 import {
   Box,
@@ -6,56 +13,93 @@ import {
   Collapse,
   FormControlLabel,
   MenuItem,
+  SxProps,
   TextField,
 } from '@mui/material';
 import { isAddress } from 'ethers/lib/utils';
 import { mainnet } from 'wagmi';
 import { isJSON } from '@app/utils/string';
+import { Resource } from '@app/types';
+import { useAppSelector } from '@app/redux/hooks';
+import { AddressTextField } from './AddressTextField';
+import { AbiTextField } from './AbiTextField';
 import { useFetchAbi } from './hooks/useFetchAbi';
 
-export const ConfigureContractForm = () => {
-  const [name, setName] = useState('');
-  const [chainId, setChainId] = useState<number>(mainnet.id);
-  const [address, setAddress] = useState('');
-  const [abi, setAbi] = useState('');
-  const [isProxy, setIsProxy] = useState(false);
-  const [logicAddress, setLogicAddress] = useState('');
-  const [logicAbi, setLogicAbi] = useState('');
+type ConfigureContractFormProps = {
+  formId: string;
+  onSubmit: (resource: Pick<Resource, 'type' | 'name' | 'metadata'>) => void;
+};
 
-  useFetchAbi({ address, chainId, onSuccess: setAbi });
-  useFetchAbi({ address: logicAddress, chainId, onSuccess: setLogicAbi });
+type FormContainerProps = {
+  children: ReactNode;
+  sx?: SxProps;
+};
 
-  const isValidAddress = useMemo(
-    () => !address || isAddress(address),
-    [address]
+export const FormContainer = ({ children, sx }: FormContainerProps) => {
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, ...sx }}>
+      {children}
+    </Box>
+  );
+};
+
+export const ConfigureContractForm = ({
+  formId,
+  onSubmit,
+}: ConfigureContractFormProps) => {
+  const { activeResource } = useAppSelector((state) => state.resources);
+
+  const [name, setName] = useState(activeResource?.name ?? '');
+  const [chainId, setChainId] = useState<number>(
+    activeResource?.metadata.smartContract?.chainId ?? mainnet.id
+  );
+  const [address, setAddress] = useState(
+    activeResource?.metadata.smartContract?.address ?? ''
+  );
+  const [abi, setAbi] = useState(
+    activeResource?.metadata.smartContract?.abi ?? ''
+  );
+  const [isProxy, setIsProxy] = useState(
+    activeResource?.metadata.smartContract?.isProxy ?? false
+  );
+  const [logicAddress, setLogicAddress] = useState(
+    activeResource?.metadata.smartContract?.logicAddress ?? ''
+  );
+  const [logicAbi, setLogicAbi] = useState(
+    activeResource?.metadata.smartContract?.logicAbi ?? ''
   );
 
-  const isValidAbi = useMemo(() => !abi || isJSON(abi), [abi]);
+  const { error: fetchAbiError, isLoading: isLoadingAbi } = useFetchAbi({
+    abi,
+    address,
+    chainId,
+    onAbiChange: setAbi,
+  });
+  const { error: fetchLogicAbiError, isLoading: isLoadingLogicAbi } =
+    useFetchAbi({
+      abi: logicAbi,
+      address: logicAddress,
+      chainId,
+      onAbiChange: setLogicAbi,
+    });
 
-  const isValidLogicAddress = useMemo(
-    () => !logicAddress || isAddress(logicAddress),
-    [logicAddress]
+  const isAbiShown = useMemo(
+    () => isAddress(address) && !fetchAbiError && !isLoadingAbi,
+    [address, fetchAbiError, isLoadingAbi]
   );
-
-  const isValidLogicAbi = useMemo(
-    () => !logicAbi || isJSON(logicAbi),
-    [logicAbi]
+  const isLogicAbiShown = useMemo(
+    () => isAddress(logicAddress) && !fetchLogicAbiError && !isLoadingLogicAbi,
+    [logicAddress, fetchLogicAbiError, isLoadingLogicAbi]
   );
 
   const handleNameChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value);
   }, []);
 
-  const handleNetworkChange = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      setChainId(parseInt(e.target.value, 10));
-    },
-    []
-  );
-
   const handleAddressChange = useCallback(
-    async (e: ChangeEvent<HTMLInputElement>) => {
+    (e: ChangeEvent<HTMLInputElement>) => {
       setAddress(e.target.value);
+      setAbi('');
     },
     []
   );
@@ -65,8 +109,9 @@ export const ConfigureContractForm = () => {
   }, []);
 
   const handleLogicAddressChange = useCallback(
-    async (e: ChangeEvent<HTMLInputElement>) => {
+    (e: ChangeEvent<HTMLInputElement>) => {
       setLogicAddress(e.target.value);
+      setLogicAbi('');
     },
     []
   );
@@ -78,6 +123,15 @@ export const ConfigureContractForm = () => {
     []
   );
 
+  const handleNetworkChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setChainId(parseInt(e.target.value, 10));
+      setAbi('');
+      setLogicAbi('');
+    },
+    []
+  );
+
   const handleIsProxyChange = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => {
       setIsProxy(e.target.checked);
@@ -85,9 +139,39 @@ export const ConfigureContractForm = () => {
     []
   );
 
-  return (
-    <>
-      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+  const handleSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      if (!isAddress(address) || !isJSON(abi)) {
+        return;
+      }
+
+      if (isProxy && (!isAddress(logicAddress) || !isJSON(logicAbi))) {
+        return;
+      }
+
+      onSubmit({
+        type: 'smart_contract',
+        name,
+        metadata: {
+          smartContract: {
+            chainId,
+            address,
+            abi,
+            isProxy,
+            logicAddress: isProxy ? logicAddress : undefined,
+            logicAbi: isProxy ? logicAbi : undefined,
+          },
+        },
+      });
+    },
+    [onSubmit, name, chainId, address, abi, isProxy, logicAddress, logicAbi]
+  );
+
+  const primaryInputs = useMemo(
+    () => (
+      <>
         <TextField
           label="Name"
           size="small"
@@ -102,6 +186,9 @@ export const ConfigureContractForm = () => {
           size="small"
           select
           required
+          inputProps={{
+            'data-testid': 'configure-contract-form-network-select',
+          }}
         >
           {CHAINS.map((chain) => (
             <MenuItem key={chain.id} value={chain.id}>
@@ -109,70 +196,102 @@ export const ConfigureContractForm = () => {
             </MenuItem>
           ))}
         </TextField>
-        <TextField
-          label="Address"
-          size="small"
+        <AddressTextField
           value={address}
+          label="Address"
           onChange={handleAddressChange}
-          error={!isValidAddress}
-          helperText={!isValidAddress && 'Invalid address'}
+          fetchAbiError={fetchAbiError}
+          size="small"
           required
         />
-        <TextField
-          multiline
-          label="ABI"
-          size="small"
+      </>
+    ),
+    [
+      name,
+      handleNameChange,
+      chainId,
+      handleNetworkChange,
+      address,
+      handleAddressChange,
+      fetchAbiError,
+    ]
+  );
+
+  const secondaryInputs = useMemo(
+    () => (
+      <>
+        <AbiTextField
           value={abi}
-          error={!isValidAbi}
-          helperText={!isValidAbi && 'Invalid JSON'}
+          label="ABI"
           onChange={handleAbiChange}
-          maxRows={10}
-          InputProps={{
-            style: {
-              fontFamily: 'monospace',
-            },
-          }}
+          size="small"
           required
+          fullWidth
         />
         <FormControlLabel
-          control={<Checkbox value={isProxy} onChange={handleIsProxyChange} />}
-          label="Proxy?"
-        />{' '}
-      </Box>
-      <Collapse in={isProxy}>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            gap: 2,
-            marginTop: 2,
-          }}
-        >
-          <TextField
-            label="Logic Address"
-            size="small"
-            value={logicAddress}
-            onChange={handleLogicAddressChange}
-            error={!isValidLogicAddress}
-            helperText={!isValidLogicAddress && 'Invalid address'}
-          />
-          <TextField
-            multiline
-            label="Logic ABI"
-            size="small"
+          control={
+            <Checkbox
+              checked={isProxy}
+              onChange={handleIsProxyChange}
+              inputProps={{
+                // @ts-ignore data-testid should be defined as a valid prop
+                'data-testid': 'configure-contract-form-proxy-checkbox',
+              }}
+            />
+          }
+          label="This is a proxy contract"
+          componentsProps={{ typography: { variant: 'body2' } }}
+        />
+      </>
+    ),
+    [abi, handleAbiChange, handleIsProxyChange, isProxy]
+  );
+
+  const proxyInputs = useMemo(
+    () => (
+      <>
+        <AddressTextField
+          value={logicAddress}
+          label="Logic Address"
+          onChange={handleLogicAddressChange}
+          fetchAbiError={fetchLogicAbiError}
+          size="small"
+          required={isProxy}
+        />
+        <Collapse in={isLogicAbiShown}>
+          <AbiTextField
             value={logicAbi}
-            error={!isValidLogicAbi}
-            helperText={!isValidLogicAbi && 'Invalid JSON'}
+            label="Logic ABI"
             onChange={handleLogicAbiChange}
-            maxRows={10}
-            InputProps={{
-              style: {
-                fontFamily: 'monospace',
-              },
-            }}
+            size="small"
+            fullWidth
+            required={isProxy}
           />
-        </Box>
+        </Collapse>
+      </>
+    ),
+    [
+      fetchLogicAbiError,
+      handleLogicAbiChange,
+      handleLogicAddressChange,
+      isLogicAbiShown,
+      isProxy,
+      logicAbi,
+      logicAddress,
+    ]
+  );
+
+  return (
+    <form id={formId} onSubmit={handleSubmit}>
+      <FormContainer>{primaryInputs}</FormContainer>
+      <Collapse in={isAbiShown}>
+        <FormContainer sx={{ marginTop: 1 }}>
+          <FormContainer>{secondaryInputs}</FormContainer>
+          <Collapse in={isProxy}>
+            <FormContainer>{proxyInputs}</FormContainer>
+          </Collapse>
+        </FormContainer>
       </Collapse>
-    </>
+    </form>
   );
 };
