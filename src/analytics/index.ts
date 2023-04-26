4720d6cc9e147c11353e010651d262a2d6a8fbf0
achang97 <andrew@acalabs.xyz>
2023-04-26 10:17:58 -0700

    [Trivial] Disable Segment in development env (#109)

--- src/analytics/index.ts
+++ src/analytics/index.ts
@@ -1,5 +1,9 @@
 import { AnalyticsBrowser } from '@segment/analytics-next';
 
+// NOTE: We don't want to log events when running the app locally.
+const writeKey =
+  process.env.NODE_ENV !== 'development' ? process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY : '';
+
 export const analytics = AnalyticsBrowser.load({
-  writeKey: process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY ?? '',
+  writeKey: writeKey ?? '',
 });
