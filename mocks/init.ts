acfd6aafc165f00ecf98dd1c19c5b9f46a0c627e
Omkar Rajam <omkarrajam09@gmail.com>
2023-04-28 23:39:16 +0530

    Fixed "no handler" console warning coming from MSW (#120)
    MSW "captured a request without a matching request handler" console warning fix

--- mocks/init.ts
+++ mocks/init.ts
@@ -1,10 +1,18 @@
+import { API_BASE_URL } from '@app/constants';
+
 const initMocks = async () => {
   if (typeof window === 'undefined') {
     const { server } = await import('./server');
     server.listen();
   } else {
     const { worker } = await import('./browser');
-    worker.start();
+    worker.start({
+      onUnhandledRequest: (req, print) => {
+        if (req.url.href.includes(API_BASE_URL)) {
+          print.warning();
+        }
+      },
+    });
   }
 };
 
